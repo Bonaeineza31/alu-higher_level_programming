3408bc6c9de898df0176b3d9b2eef4804b450357
Bonaeineza31 <b.ineza@alustudent.com>
2024-10-21 07:05:15 -0700

    task 12#

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b < 0:
+        result = 1 / a
+        for _ in range(abs(b) - 1):
+            result /= a
+        return result
+    else:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
