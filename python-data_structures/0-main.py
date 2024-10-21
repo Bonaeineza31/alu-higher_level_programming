661af8d32182dc8180255d2590b09d72b0606210
Bonaeineza31 <b.ineza@alustudent.com>
2024-10-21 10:00:43 -0700

    task 13

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-list = __import__('0-print_list_integer.py').print_list_integer
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+list = __import__('0-print_list_integer.py')
