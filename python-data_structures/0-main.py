003fa9b688dc4d568e27a93b495eaae449571406
Bonaeineza31 <b.ineza@alustudent.com>
2024-10-21 10:10:32 -0700

    task 13

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -1,2 +1,5 @@
-#!/usr/bin/python3
-list = __import__('0-print_list_integer.py')
+#!/usr/bin/env python3
+list = __import__('0-print_list_integer')
+my_list = [1, 2, 3, 4, 5]
+result = list.print_list_integer(my_list)
+print(result)
--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{:d}".format(item))
+    return '\n'.join("{:d}".format(item) for item in my_list)
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
