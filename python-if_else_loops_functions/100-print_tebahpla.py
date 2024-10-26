5d564fad44cfa30e8eebd3f0d26d0d41a12e8192
Bonaeineza31 <b.ineza@alustudent.com>
2024-10-26 13:24:07 -0700

    task 100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print("".join("{}".format(chr(c) if i % 2 == 0 else chr(c).upper())
-              for i, c in enumerate(range(ord('z'), ord('a') - 1, -1))), end="")
+for i, c in enumerate(range(ord('z'), ord('a') - 1, -1))), end="")
