993476289d9b94a8859df90bd21c8cf767cd68ae
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-03 11:16:49 +0000

    task 00#

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except IndexError:
             break
-    print()  # Print newline after the loop
+    print()
     return count
