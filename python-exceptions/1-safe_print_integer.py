e6c97faa62fa4267969fe7676fefc1d6acbd4c5a
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-04 13:16:40 +0000

    task 2

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        # Attempt to print the value as an integer
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-<<<<<<< HEAD
-        return False
-=======
         # If there's an error in formatting, return False
         return False
->>>>>>> refs/remotes/origin/master
