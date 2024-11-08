1a9e6d98e4128e98ba3f998a66debb91ccc4ba96
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-08 23:23:01 +0200

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module defines a Square class with a private instance attribute `size`,
-includes validation for `size`, and has a method to compute the area of the square.
+includes validation for `size`, and a method to compute it's areaS.
 """
 
 
@@ -26,7 +26,7 @@ class Square:
             ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+          raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
@@ -38,4 +38,4 @@ class Square:
         Returns:
             int: The area of the square (size * size).
         """
-        return self.__size ** 2
+        return self.__size **2
