56a09c4bcfcd0c5d0e796ea9f32c1f6a19d667fe
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-13 15:54:25 +0200

    updated

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script adds all arguments passed to it to a
+to a JSON file named 'add_item.json'. 
+"""
+
+
+import sys
+from os import path
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+if path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
