7b2a985abb4f989f99b49aa07e4d664c9ea54d64
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-13 13:48:58 +0200

    updated

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -6,6 +6,7 @@ This module provides a way to convert object to its JSON string representation.
 
 import json
 
+
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (string)."""
     return json.dumps(my_obj)
