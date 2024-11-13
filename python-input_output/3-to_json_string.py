f763fa4219f8eb153e0a7efdf13ca12e2f361cf3
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-13 13:41:27 +0200

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module provides a function to append a string to a text file
-and return the number of characters added.
+This module provides a way to convert object to its JSON string representation.
 """
 
 
-def append_write(filename="", text=""):
-    """Appends a string text file and returns nbr of characters added."""
-    with open(filename, "a", encoding="utf-8") as file:
-        return file.write(text)
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)."""
+    return json.dumps(my_obj)
