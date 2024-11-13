780da8e69877aed686f69259fd0f494bf721b11f
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-13 13:34:38 +0200

    updated

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,6 +4,7 @@ This module provides a function to append a string to a text file
 and return the number of characters added.
 """
 
+
 def append_write(filename="", text=""):
     """Appends a string text file and returns nbr of characters added."""
     with open(filename, "a", encoding="utf-8") as file:
