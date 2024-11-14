ef9c0bdea7e059a7b3e4db42ad839fe9dedd4610
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-14 23:01:07 +0200

    updated

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,38 +1,17 @@
 #!/usr/bin/python3
-"""
-This module defines the Student class with attributes and a method
-to retrieve its dictionary representation for JSON serialization,
-with an optional filter on specific attributes.
-"""
+"""Write a class Student that defines a student """
 
 
 class Student:
-    """
-    Represents a student with first name, last name, and age attributes.
-    """
-
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance with first_name, last_name, and age.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
-        If `attrs` is a list of strings, only those attributes in the list
-        are included in the result. Otherwise, all attributes are included.
-        Args:
-            attrs (list): A list of attribute names to retrieve.
-        Returns:
-            dict: A dictionary of the instance's attributes.
-        """
-        if isinstance(attr, list) and all(isinstance(attr, str) for attr in attr):
-            return {attr: getattr(self, attr) for attr in attr if hasattr(self, attr)}
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {y: getattr(self, y) for y in attrs if hasattr(self, y)}
         return self.__dict__
