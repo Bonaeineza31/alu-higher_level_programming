f2058595e23bc4adaa676fa50cb9d33848902c2b
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-16 01:25:15 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,23 @@ import sys
 
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+=======
+  save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+  load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+>>>>>>> Stashed changes
 
-    try:
+  try:
         items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
+  except FileNotFoundError:
         items = []
+<<<<<<< Updated upstream
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
+=======
+  items.extend(sys.argv[1:])
+  save_to_json_file(items, "add_item.json")
+>>>>>>> Stashed changes
