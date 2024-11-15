0beac15d2a44db13909340291f28c93ee3189339
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-16 01:28:53 +0200

    Update 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,12 @@
 -- This is a script that Import the database dump of hbtn_0d_tvshows to my MySQL server
-SELECT 
+    SELECT 
     tv_shows.title, 
-    genres.name 
+    tv_genres.name
 FROM 
     tv_shows
 LEFT JOIN 
     tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN 
-    genres ON tv_show_genres.genre_id = genres.id
+    tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY 
-    tv_shows.title ASC, genres.name ASC;
+    tv_shows.title ASC, tv_genres.name ASC;
