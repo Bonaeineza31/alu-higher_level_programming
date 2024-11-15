e4381ffa869ade698e9a80f3413ae4850327a62f
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-16 01:32:01 +0200

    updated

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,13 +1,7 @@
 -- This is a script that Import the database dump of hbtn_0d_tvshows to my MySQL server
-SELECT 
-    genres.name
-FROM 
-    tv_shows
-JOIN 
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN 
-    genres ON tv_show_genres.genre_id = genres.id
-WHERE 
-    tv_shows.title = 'Dexter'
-ORDER BY 
-    genres.name ASC
+SELECT tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
