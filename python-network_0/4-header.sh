d33d2d6f8ea8600e166327b8f6707bc7a199205d
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-20 21:26:34 +0000

    task 04

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Bash script that takes  an argument, sends a GET request to the URL, and displays the response
+curl -sH "X-School-User-Id: 98" "$1"
