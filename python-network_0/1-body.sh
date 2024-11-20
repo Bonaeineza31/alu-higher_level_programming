ddb897f1e5ad71646da6d10b756b32e73b637187
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-20 19:13:54 +0000

    task 01

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s -o response.txt -w "%{http_code}" "$1" | grep -q "200" && cat response.txt
-
