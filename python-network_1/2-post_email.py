a20b85461bd43dd025b11d8864df3a3e9a0e0003
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-23 00:06:32 +0200

    updated

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,15 +3,16 @@
 A script that sends a POST request with an email as a parameter
 and displays the body of the response (decoded in utf-8).
 """
-
+import sys
 import urllib.parse
 import urllib.request
-import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    with urllib.request.urlopen(url, data) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
