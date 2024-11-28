5fac74ae06eb34aac15d3412681bbed8102837e9
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-28 13:03:58 -0800

    task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,28 +10,28 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    # Create an engine to connect to the database
+    # Create database connection
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            mysql_username, mysql_password, database_name
-        ),
-        pool_pre_ping=True,
+        f"mysql+mysqldb://{username}:{password}@localhost/{db_name}",
+        pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
 
-    # Create a session maker and initialize a session
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and delete all states containing 'a'
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    # Query and delete states with 'a' in their name
+    states_to_delete = session.query(State).filter(
+        State.name.like('%a%')
+    ).all()
+
     for state in states_to_delete:
         session.delete(state)
-    session.commit()
 
-    # Close the session
-    session.close()
+    session.commit()  # Commit changes to the database
+    session.close()  # Close the session
