1ad6f9face54777a4663fed0a03f9d04a75bba86
Bonaeineza31 <b.ineza@alustudent.com>
2024-11-28 13:12:44 -0800

    added

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,17 @@
 #!/usr/bin/python3
-"""
-1-filter_states.py
-Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
-"""
+"""Lists states"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Get arguments from the command line
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
-
-    # Execute the SQL query with a BINARY condition for case sensitivity
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch and display the results
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswith("N"):
+            print(row)
     cur.close()
-    db.close()
+    conn.close()
--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-Deletes all State objects with a name containing the letter 'a' from the
-database hbtn_0e_6_usa.
-"""
-
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Create database connection
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost/{db_name}",
-        pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Query and delete states with 'a' in their name
-    states_to_delete = session.query(State).filter(
-        State.name.like('%a%')
-    ).all()
-
-    for state in states_to_delete:
-        session.delete(state)
-
-    session.commit()  # Commit changes to the database
-    session.close()  # Close the session
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
+    session.commit()
